491a10441cd1174659d5312ff20ff233375f59ee
Praful Gupta <prafulgupta6@gmail.com>
2020-10-01 23:55:49 +0530

    Test Fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'Screen1.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MainAppInsert());
 
-class MyApp extends StatelessWidget {
+class MainAppInsert extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
